ca885144a778370505aba657520752fc798a00a4
Sagar Maurya <sagarmaurya@asmigar.com>
2023-08-02 08:39:10 +0530

    update server log * to print hostname * open link on browser directly from terminal

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+const hostname = "127.0.0.1"
 const port = 3000
 
 app.get('/', (req, res) => {
@@ -7,5 +8,5 @@ app.get('/', (req, res) => {
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+    console.log(`Server running at http://${hostname}:${port}/`);
 })
