a383d46337ae1f39d4e39779c343bae88f9b5e59
Sagar Maurya <sagarmaurya@asmigar.com>
2023-08-02 08:21:45 +0530

    introduce express

--- app.js
+++ app.js
@@ -1,21 +1,11 @@
-const http = require('http');
+const express = require('express')
+const app = express()
+const port = 3000
 
-const hostname = '127.0.0.1';
-const port = 3000;
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  if (req.url.includes('/user')){
-	res.end(`Hello ${req.url.split('/').reverse()[0]}`)
-  } else if (req.url.includes('/org')){
-	res.end(`Hello World with love from ${req.url.split('/').reverse()[0]}`)
-  } else {
-	
-  	res.end('Hello World');
-  }
-});
-
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
