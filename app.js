8b88e323114cf22863d1579e0ec1ea2b294d1383
Sagar Maurya <sagarmaurya@asmigar.com>
2023-08-03 07:11:35 +0530

    greet user

--- app.js
+++ app.js
@@ -7,6 +7,10 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+app.get('/user/:username', (req, res) => {
+  res.send(`Hello ${req.params.username}!`)
+})
+
 app.listen(port, () => {
     console.log(`Server running at http://${hostname}:${port}/`);
 })
